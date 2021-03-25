ded0642372e2f6085233a935a41a184f6382b0ac
mili12321 <milita.malzev@gmail.com>
2021-03-25 13:31:27 +0200

    V

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
   dbURL:
-    // 'mongodb+srv://movigos:movigos@cluster0.h9xmp.mongodb.net/<dbname>?retryWrites=true&w=majority',
     'mongodb+srv://mili:123456mili@cluster0.p3vez.mongodb.net/<dbname>?retryWrites=true&w=majority',
 }
