d917b848896f0e8f94b53760fa11fce1b1588828
mili12321 <milita.malzev@gmail.com>
2021-04-16 13:33:12 +0300

    V

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -3,9 +3,18 @@ module.exports = connectSockets
 
 function connectSockets(io) {
     io.on('connection', (socket) => {
-      console.log('connected');
-      socket.on('disconnect', function(){
-        console.log('disconnected');
+
+      socket.on('message', (data) => {
+        console.log('data',data);
+        io.emit('message', data);
+        // socket.broadcast.emit("test", "hello friends!");
+        // socket.emit('test', "hello!");
+      })
+      socket.on("test", (data) => {
+        console.log("test");
+        io.emit('test', {
+         text:'123'
+        })
       });
     })
 }
